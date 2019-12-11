2ce17637d739b40dbdf900105a97ce39d368c156
Santi Barbat <barbatsan@gmail.com>
2019-12-11 20:13:50 +0000

    Remove reset scroll

--- resources/js/views/App.vue
+++ resources/js/views/App.vue
@@ -1,7 +1,5 @@
 <template>
     <div class="flex flex-wrap mx-auto antialiased leading-tight">
-        <div id="reset-scroll"></div>
-
         <alert></alert>
         
         <app-header></app-header>
