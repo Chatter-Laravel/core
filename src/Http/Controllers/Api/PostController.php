e22afd8951adb152713d1c79aa47a8343ed61195
Santi Barbat <barbatsan@gmail.com>
2019-12-11 13:11:00 +0000

    Post order by

--- src/Http/Controllers/Api/PostController.php
+++ src/Http/Controllers/Api/PostController.php
@@ -35,6 +35,7 @@ class PostController extends Controller
             $discussion = model(DiscussionInterface::class)::where('slug', $request->discussion)->first();
 
             return new PostCollection($discussion->posts()
+                ->orderBy('created_at')
                 ->paginate(config('chatter.paginate.discussions')));
         }
 
