ca3723ec3246d16fe6f735aa6037f57dcc0c0bb6
Santi Barbat <barbatsan@gmail.com>
2019-12-16 19:58:05 +0000

    Missing migration

---
+++ database/migrations/2017_01_16_121747_create_chatter_user_discussion_pivot_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateChatterUserDiscussionPivotTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('chatter_user_discussion', function (Blueprint $table) {
+            $table->unsignedBigInteger('user_id')->index();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            
+            $table->unsignedBigInteger('discussion_id')->index();
+            $table->foreign('discussion_id')->references('id')->on('chatter_discussions')->onDelete('cascade');
+            
+            $table->primary(['user_id', 'discussion_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('chatter_user_discussion');
+    }
+}
