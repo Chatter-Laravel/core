015db7f9658e95734f8458a3f56f1592ee0648b8
Santiago Barbat <barbatsan@gmail.com>
2021-10-30 16:38:27 +0100

    Margin between emoji and reaction count

--- resources/js/components/Reactions.vue
+++ resources/js/components/Reactions.vue
@@ -1,56 +1,58 @@
 <template>
-    <div class="flex items-center text-gray-700">
-        <div v-for="r in post.reactions" :key="r.id" @click="toggle({ name: r.emoji_name, emoji: r.emoji })" :class="{ 'bg-blue-100 hover:bg-blue-300 border border-2 border-blue-500': r.user_reacted, 'bg-gray-200 hover:bg-gray-300': !r.user_reacted }" class="px-2 py-1 mr-1 text-center text-sm text-gray-700 rounded-full cursor-pointer">
-            {{ r.emoji }}{{ r.count }}
-        </div>
-
-        <emoji-picker
-            v-if="auth"
-            :post="post"
-        ></emoji-picker>
+  <div class="flex items-center text-gray-700">
+    <div v-for="r in post.reactions" :key="r.id" @click="toggle({ name: r.emoji_name, emoji: r.emoji })"
+         :class="{ 'bg-blue-100 hover:bg-blue-300 border border-2 border-blue-500': r.user_reacted, 'bg-gray-200 hover:bg-gray-300': !r.user_reacted }"
+         class="px-2 py-1 mr-1 text-center text-sm text-gray-700 rounded-full cursor-pointer">
+      {{ r.emoji }}<span class="ml-1">{{ r.count }}</span>
     </div>
+
+    <emoji-picker
+        v-if="auth"
+        :post="post"
+    ></emoji-picker>
+  </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex';
+import {mapGetters} from 'vuex';
 import EmojiPicker from './EmojiPicker'
-import { events } from '../events'
+import {events} from '../events'
 
 export default {
-    components: {
-        EmojiPicker
-    },
-    props: {
-        post: Object
-    },
-    mounted() {
-        var self = this
+  components: {
+    EmojiPicker
+  },
+  props: {
+    post: Object
+  },
+  mounted() {
+    const self = this
 
-        window.ChatterEvents.$on(events.EMOJI_CHANGED, ({emoji, post}) => {
-            if (self.post.id === post.id) {
-                self.toggle(emoji)
-            }
-        })
-    },
-    computed: {
-        ...mapGetters([ 'auth' ])
-    },
-    methods: {
-        toggle(emoji) {
-            var self = this;
+    window.ChatterEvents.$on(events.EMOJI_CHANGED, ({emoji, post}) => {
+      if (self.post.id === post.id) {
+        self.toggle(emoji)
+      }
+    })
+  },
+  computed: {
+    ...mapGetters(['auth'])
+  },
+  methods: {
+    toggle(emoji) {
+      const self = this;
 
-            axios.post('/api/chatter/post/react/' + this.post.id, {
-                emoji_name: emoji.name,
-                emoji: emoji.emoji
-            })
-            .then(response => {
-                self.post.reactions = response.data.reactions
-            })
-            .catch(error => {
-                console.error(error)
-            })
-        }
+      axios.post('/api/chatter/post/react/' + this.post.id, {
+        emoji_name: emoji.name,
+        emoji: emoji.emoji
+      })
+          .then(response => {
+            self.post.reactions = response.data.reactions
+          })
+          .catch(error => {
+            console.error(error)
+          })
     }
+  }
 }
 </script>
 
@@ -110,11 +112,11 @@ export default {
 }
 
 .heartBeat {
-    -webkit-animation-name: heartBeat;
-    animation-name: heartBeat;
-    -webkit-animation-duration: .75s;
-    animation-duration: .75s;
-    -webkit-animation-timing-function: ease-in-out;
-    animation-timing-function: ease-in-out;
+  -webkit-animation-name: heartBeat;
+  animation-name: heartBeat;
+  -webkit-animation-duration: .75s;
+  animation-duration: .75s;
+  -webkit-animation-timing-function: ease-in-out;
+  animation-timing-function: ease-in-out;
 }
 </style>
